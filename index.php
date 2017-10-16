627e16e6de4428ad81a696946fc423213a3d18fd
KodBagarn <korveniglaset@gmail.com>
2017-10-16 14:12:12 +0200

    start

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+
+  </head>
+  <body>
+    <?php
+      include("header.php");
+    ?>
+    <main>
+
+    </main>
+    <?php
+     include("footer.php");
+    ?>
+  </body>
+</html>
