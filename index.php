cd9d66b007afe0b5dfdc5fabff68b2db73805dc8
KodBagarn <korveniglaset@gmail.com>
2017-10-25 11:40:34 +0200

    abo

--- index.php
+++ index.php
@@ -10,16 +10,16 @@
       ?>
     </header>
     <main>
-<<<<<<< HEAD
+
       <h2>Test</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-=======
+
       <h1>Testrubrik</h1>
       <p>hej</p>
->>>>>>> 7ab36e911e7f8b513c6145dcad4f196796e8d7c2
+
     </main>
     <?php
      include("footer.php");
