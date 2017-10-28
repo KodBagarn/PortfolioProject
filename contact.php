9eb47a3351c553f254faada2d83c7747247d1445
karinnlinnea <32543559+karinnlinnea@users.noreply.github.com>
2017-10-28 16:04:52 +0200

    Content on Contact + Home

--- contact.php
+++ contact.php
@@ -14,18 +14,40 @@
     </header>
     <main>
       <h2>Contact us</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+      <div id="contactpage">
+        <div id="contactleft">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <li>
+              Facebook: <a class="sociallinks" href="https://www.facebook.com/">https://www.facebook.com/</a>
+            </li>
+            <li>
+              Instagram: <a class="sociallinks" href="https://www.instagram.com/">https://www.instagram.com/</a>
+            </li>
+            <li>
+              Youtube: <a class="sociallinks" href="https://www.youtube.com/">https://www.youtube.com/</a>
+            </li>
+            <li>
+              Twitter: <a class="sociallinks" href="https://www.twitter.com/">https://www.twitter.com/</a>
+            </li>
+          </p>
+        </div>
+
+        <div id="contactright">
+          <form id="contactform" action="" method="">
+            <h2>Send us an e-mail!</h2>
+            <h3 class="contacth3">Full name</h3>
+            <input type="text" name="fullname" value="">
+            <h3 class="contacth3">E-mail</h3>
+            <input type="email" name="email" value="">
+            <br><br>
+            <textarea name="name" rows="9" cols="60"></textarea>
+            <br>
+            <input id="contactsubmit" type="submit" name="submit" value="Send">
+          </form>
+        </div>
+      </div>
     </main>
     <?php
      include("footer.php");
