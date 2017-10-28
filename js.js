5e97f8656435221d0692af3f619c0dc274a76ad6
KodBagarn <korveniglaset@gmail.com>
2017-10-28 20:33:08 +0200

    hej

--- js.js
+++ js.js
@@ -2,9 +2,17 @@ $(document).ready( function() {
 	var icon = $(".hamburger");
 	var menu = $(".topnav");
 
-	icon.click( function(event) {
+	icon.click(function(event) {
 		event.preventDefault();
 		icon.toggleClass("open");
 		menu.toggleClass("open");
 	} );
-} );
+
+	/*if (icon.toggleClass("open")= true) {
+		menu.hover(function(event){
+			even.preventDefault();
+			icon.toggleClass("open");
+			menu.toggleClass("open");
+		});
+	} */
+});
