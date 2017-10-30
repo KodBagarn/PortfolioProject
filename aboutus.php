ec6d3f5b9aec0fbe7be47b68eb763bfad95def93
KodBagarn <korveniglaset@gmail.com>
2017-10-30 11:06:17 +0100

    hej

--- aboutus.php
+++ aboutus.php
@@ -15,7 +15,7 @@
     <main>
       <h2>About us</h2>
       <h4>Vision</h4>
-      <p>
+      <p class="aboutusp">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -24,7 +24,7 @@
       <img class="aboutusimg" src="img/vision.png">
 
       <h4>Organisation</h4>
-      <p>
+      <p class="aboutusp">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
