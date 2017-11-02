f87e25fafa5f7f8a0b432463c338a70b82e5a0f2
KodBagarn <korveniglaset@gmail.com>
2017-11-02 17:57:12 +0100

    footer pic and general fix

--- footer.php
+++ footer.php
@@ -5,4 +5,5 @@
   <a href="https://www.youtube.com/"><img src="img/youtubeicon.png" alt=""></a>
   <a href="https://www.twitter.com/"><img src="img/twittericon.png" alt=""></a>
   <a href="mailto:example@gmail.com?"><img src="img/mailicon.png" alt=""></a>
+  <img id="logofooter" src="img/logo.png">
 </footer>
